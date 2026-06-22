921c559df2af47804cf29d1b4f73d758207f36b4
nawal886 <nawalharomaen@gmail.com~>
2026-06-22 15:41:39 +0700

    yem

--- config/cors.go
+++ config/cors.go
@@ -3,6 +3,7 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:5173",
 	// "https://belatihanfix-production.up.railway.app",
+	"https://my-fe-teal.vercel.app",
 }
 
 func GetAllowedOrigins() []string {
